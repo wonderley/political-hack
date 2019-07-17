65d6b2a04ffe03cc01e5f8bae11444f0c3433e77
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2019-07-17 18:32:38 -0400

    Improve error handling

--- app/src/App.js
+++ app/src/App.js
@@ -2,12 +2,95 @@ import React from 'react';
 import './App.css';
 import Reps from './Reps';
 
-function App() {
-  return (
-    <div className="App">
-      <Reps address='3423 Piedmont Rd NE, Atlanta, GA 30305'/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    const address = '3423 Piedmont Rd NE, Atlanta, GA 30305';
+    this.state = {
+      address,
+      reps: [],
+      error: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+       address: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ reps: [] });
+    this.fetchReps();
+  }
+
+  componentDidMount() {
+    this.fetchReps();
+  }
+
+  render() {
+    let content;
+    if (this.state.error) {
+      content = (
+        <div>
+          {this.state.error}
+        </div>
+      );
+    } else {
+      content = (
+        <Reps reps={this.state.reps}/>
+      );
+    }
+    return (
+      <div className="App">
+        <h1>Representatives at
+          <form onSubmit={this.handleSubmit}>
+            <input type="text"
+                   onChange={this.handleChange}
+                   defaultValue={this.state.address}
+                   style={{'line-height': '4em'}}/>
+          </form>
+        </h1>
+        {content}
+      </div>
+    );
+  }
+
+  fetchReps() {
+    debugger;
+    const repsForAddressUrl = encodeURI(`/beta/repsForAddress/${this.state.address}`);
+    const that = this;
+    fetch(repsForAddressUrl)
+    .then(response => {
+      debugger;
+      if (response.status !== 200) {
+        throw new Error(`Request for url ${repsForAddressUrl} failed with status ${response.status}.`);
+      }
+      return response.json();
+    })
+    .then(res => {
+      debugger;
+      console.log(`URL ${repsForAddressUrl} fetched successfully`);
+      const reps = res.data.officials;
+      // Assign office names to the appropriate reps
+      res.data.offices.forEach(office => {
+        office.officialIndices.forEach(officialIdx => {
+          reps[officialIdx] = {
+            ...reps[officialIdx],
+            officeName: office.name,
+          }
+        }); 
+      });
+      that.setState({ reps, error: '' });
+    })
+    .catch(err => {
+      console.error(err.toString());
+      that.setState({ error: 'No reps were found for that address. Make sure you enter a full address including city and zip code.' });
+    });
+  }
 }
 
 export default App;
--- app/src/Reps.js
+++ app/src/Reps.js
@@ -6,40 +6,12 @@ class Reps extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      reps: [],
       // ID of the item most recently hovered
       hoverId: -1,
     };
   }
   
   componentDidMount() {
-    const repsForAddressUrl = encodeURI(`/beta/repsForAddress/${this.props.address}`);
-    const that = this;
-    fetch(repsForAddressUrl)
-    .then(response => {
-      if (response.status !== 200) {
-        throw new Error(`Request for url ${repsForAddressUrl} failed with status ${response.status}.`);
-      }
-      return response.json();
-    })
-    .then(res => {
-      debugger;
-      console.log(`URL ${repsForAddressUrl} fetched successfully`);
-      const reps = res.data.officials;
-      // Assign office names to the appropriate reps
-      res.data.offices.forEach(office => {
-        office.officialIndices.forEach(officialIdx => {
-          reps[officialIdx] = {
-            ...reps[officialIdx],
-            officeName: office.name,
-          }
-        }); 
-      });
-      that.setState({ reps });
-    })
-    .catch(err => {
-      console.error(err.toString());
-    });
   }
 
   // Handle a list item being hovered over
@@ -50,7 +22,7 @@ class Reps extends React.Component {
   render() {
     const hoverId = this.state.hoverId;
     const that = this;
-    const reps = this.state.reps.map((rep, i) => {
+    const reps = this.props.reps.map((rep, i) => {
       return (
         <li key={i} 
             onMouseEnter={that.onRepMouseEnter.bind(that, i)}>
@@ -63,10 +35,8 @@ class Reps extends React.Component {
         </li>
       );
     });
-    const headerText = this.props.address ? `Representatives at ${this.props.address}` : '';
     return ( 
       <div>
-        <h1>{headerText}</h1>
         <ul>
           {reps}
         </ul>
