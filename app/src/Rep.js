a523fd6d55b4887eb4ebdd2cfc62186e044d8036
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2019-08-04 14:19:22 -0400

    Complete basic profile page

--- app/src/Rep.js
+++ app/src/Rep.js
@@ -1,28 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Rep.css';
+import placeholder from './rep-placeholder.png'; 
 
 class Rep extends React.Component {
   render() {
     const repData = this.props.location.state;
-    const { name, address, party, phones, urls,
+    const { name, address: addresses, party, phones, urls,
             photoUrl, emails, channels, officeName } = repData;
+    const repImg = photoUrl ?
+      <img src={photoUrl} alt={name}/> :
+      <img src={placeholder} alt="Rep Placeholder"/>;
+    const addressDivs = divsForAddresses(addresses);
+    const phoneDivs = phones && phones.map((phoneNumber, i) => {
+      const index = phones.length > 1 ? ` ${i + 1}` : '';
+      return <div>Phone{index}: {phoneNumber}</div>
+    });
+    const urlDivs = urls && urls.map((url, i) => {
+      const index = urls.length > 1 ? ` ${i + 1}` : '';
+      return <div>Website{index}: <a href={url} target="_blank" rel="noopener noreferrer">{url}</a></div>
+    });
+    const emailDivs = emails && emails.map((email, i) => {
+      const index = emails.length > 1 ? ` ${i + 1}` : '';
+      return <div>Email{index}: <a href={`mailto://${email}`}>{email}</a></div>
+    });
+    const socialMediaDivs = channels &&
+      channels.filter(({type, _}) => type !== 'GooglePlus')
+              .map(({type, id}) => {
+        return <div>{type}: <a href={channelUrl(type, id)} target="_blank" rel="noopener noreferrer">{id}</a></div>;
+      });
     return (
-      <div style={{
-        display: 'flex',
-        flexDirection: 'horizontal',
-        padding: '15px',
-      }}>
-        <img src={photoUrl} alt="" style={{
-          'max-height': '400px',
-          'max-width': '50%',
-          paddingRight: '15px',
-        }}/>
+      <div>
         <div style={{
-          flexDirection: 'vertical',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          paddingTop: '15px',
+        }}><h1>{name}</h1></div>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          padding: '15px',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}>
-          <div>{`Name: ${name}`}</div>
-          <div>{`Party: ${party || 'Unknown'}`}</div>
-          <div>Props: {JSON.stringify(repData)}</div>
+          <div className='imgParent'>
+            {repImg}
+          </div>
+          <div>
+            <div>{`Office: ${officeName || 'Unknown'}`}</div>
+            <div>{`Party: ${party || 'Unknown'}`}</div>
+            {phoneDivs}
+            {addressDivs}
+            {emailDivs}
+            {urlDivs}
+            {socialMediaDivs}
+          </div>
         </div>
       </div>
     );
@@ -61,6 +94,40 @@ class Rep extends React.Component {
     //     };
     //     console.log(JSON.stringify(profileProps));
     //   });
+  }
+}
+
+function divsForAddresses(addresses) {
+  return addresses && addresses.map((address, i) => {
+    const index = addresses.length > 1 ? ` ${i + 1}` : '';
+    return (
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+      }}>
+        <div>Address{index}:</div>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          padding: '0 3px 0 3px'
+        }}>
+          <div>{address.line1}</div>
+          <div>{address.line2}</div>
+          <div>{address.line3}</div>
+          <div>{address.city}, {address.state} {address.zip}</div>
+        </div>
+      </div>
+    );
+  });
+}
+
+function channelUrl(type, id) {
+  if (type === 'Facebook') {
+    return `https://facebook.com/${id}`;
+  } else if (type === 'Twitter') {
+    return `https://twitter.com/${id}`;
+  } else if (type === 'YouTube') {
+    return `https://youtube.com/channel/${id}`;
   }
 }
 
